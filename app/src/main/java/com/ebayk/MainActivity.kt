11b4db78bda3ab6bcb902c0f58d54fe52c8fdec0
Vladimir Sechkarev <sechkarevs@gmail.com>
2022-02-20 13:24:43 +0100

    use compose

--- app/src/main/java/com/ebayk/MainActivity.kt
+++ app/src/main/java/com/ebayk/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.ebayk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import com.ebayk.ui.theme.AppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AppTheme {
+                Text("Hello world!")
+            }
+        }
     }
 }
