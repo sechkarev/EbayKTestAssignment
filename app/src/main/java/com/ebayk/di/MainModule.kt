a5de9a2b4e26b661914c91fe3766c29750e59fff
Vladimir Sechkarev <sechkarevs@gmail.com>
2022-02-20 16:46:43 +0100

    trying to setup network calls, but they don't work

--- app/src/main/java/com/ebayk/di/MainModule.kt
+++ app/src/main/java/com/ebayk/di/MainModule.kt
@@ -3,6 +3,9 @@ package com.ebayk.di
 import com.ebayk.remote.Endpoints
 import com.ebayk.usecase.RetrieveApartmentInfo
 import com.ebayk.viewmodel.MainViewModel
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -12,9 +15,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 val MainModule = module {
 
     factory<Endpoints> {
+        val credentials = Credentials.basic("candidate", "yx6Xz62y")
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://gateway.ebay-kleinanzeigen.de/mobile-api/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient
+                .Builder()
+                .addInterceptor(
+                    HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    }
+                )
+                .addInterceptor {
+                    it.proceed(
+                        it.request()
+                            .newBuilder()
+                            .addHeader("authorization", credentials)
+                            .build()
+                    )
+                }
+                .build()
+            )
             .build()
 
         retrofit.create(Endpoints::class.java)
--- app/src/main/java/com/ebayk/remote/Endpoints.kt
+++ app/src/main/java/com/ebayk/remote/Endpoints.kt
@@ -1,9 +1,15 @@
 package com.ebayk.remote
 
+import okhttp3.Credentials
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface Endpoints {
 
     @GET("candidate/ads/1118635128")
-    suspend fun retrieveApartmentInfo(): Unit
+    suspend fun retrieveApartmentInfo(
+        @Header("authorization") credentials: String = Credentials.basic("candidate", "yx6Xz62y"),
+        @Header("user-agent") userAgent: String = "some-user-agent",
+        @Header("accept") accept: String = "*/*",
+    )
 }
