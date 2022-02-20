cdaafb31e855294acf0e8f4c3628e469cc052701
Vladimir Sechkarev <sechkarevs@gmail.com>
2022-02-20 13:50:36 +0100

    draw an image behind the status bar

--- app/src/main/java/com/ebayk/MainActivity.kt
+++ app/src/main/java/com/ebayk/MainActivity.kt
@@ -3,16 +3,43 @@ package com.ebayk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import coil.compose.rememberImagePainter
 import com.ebayk.ui.theme.AppTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AppTheme {
-                Text("Hello world!")
+                ProvideWindowInsets {
+                    val systemUiController = rememberSystemUiController()
+                    SideEffect {
+                        systemUiController.setSystemBarsColor(Transparent)
+                    }
+                    Image(
+                        painter = rememberImagePainter(
+                            "https://i.ebayimg.com/00/s/MTA2NlgxNjAw/z/~CoAAOSwjZJhSgCC/\$_57.JPG"
+                        ),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .height(250.dp)
+                            .fillMaxWidth()
+                    )
+                }
             }
         }
     }
